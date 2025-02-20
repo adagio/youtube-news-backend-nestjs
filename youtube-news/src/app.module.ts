a3046bd2663bbef3940a3e3a30c06740679ed2ed
Franco Cedillo <franco.cedillo@gmail.com>
2025-02-20 03:01:41 +0000

    Refactor database configuration to use PostgreSQL with dynamic settings from ConfigService

--- youtube-news/src/app.module.ts
+++ youtube-news/src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
 import { Channel } from './entities/channel.entity';
 import { Video } from './entities/video.entity';
 import { ChannelsModule } from './modules/channels/channels.module';
 import { PreloaderModule } from './modules/preloader/preloader.module';
 import { VideosModule } from './modules/videos/videos.module';
+import * as path from 'path';
 
 @Module({
   imports: [
@@ -16,11 +17,24 @@ import { VideosModule } from './modules/videos/videos.module';
       envFilePath: '.env',
       cache: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'youtube.db',
-      entities: [Channel, Video],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [Channel, Video],
+        migrations: [path.join(__dirname, './database/migrations/*{.ts,.js}')],
+        synchronize: process.env.NODE_ENV === 'development',
+        migrationsTableName: 'typeorm_migrations',
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+      inject: [ConfigService],
     }),
     ChannelsModule,
     PreloaderModule,
